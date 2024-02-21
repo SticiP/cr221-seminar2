3f84dc3126525fceb3451ef88bf2ab8d50799f92
bus1hero <petru.cervac@gmail.com>
2024-02-21 10:57:27 +0200

    update simulate metode event

--- src/Jet.java
+++ src/Jet.java
@@ -1,17 +1,22 @@
 public class Jet {
     private  int heigh;
+    private int latitude;
+    private int longitude;
 
-    public Jet(int heigh
-    ) {
+    public Jet(int heigh,
+               int latitude, int longitude) {
         this.heigh = heigh;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public void simulateMeteoEvent(MeteoEvent rain) {
         switch(rain){
             case SUN -> {
-                heigh = heigh + 5;
+                heigh = heigh + 2;
+                latitude = latitude + 10;
             }
-            case RAIN -> heigh = heigh - 5;
+            case RAIN -> latitude = latitude + 5;
         }
     }
 
@@ -19,6 +24,8 @@ public class Jet {
     public String toString() {
         return "Jet{" +
                 "heigh=" + heigh +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
                 '}';
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Jet jet = new Jet(50);
+        Jet jet = new Jet(50,100,100);
         System.out.println(jet);
         jet.simulateMeteoEvent(MeteoEvent.SUN);
         System.out.println(jet);
